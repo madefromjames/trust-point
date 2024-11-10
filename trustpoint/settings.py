ac4ac63860c6056ea6b6623813fa14860e32946c
madefromjames <madefromjames@gmail.com>
2024-11-10 21:32:55 +0100

    configuration bugs

--- trustpoint/settings.py
+++ trustpoint/settings.py
@@ -39,12 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'finance',
     # other Django apps
-    'tailwind',
-    'theme',
     'django_browser_reload'
 ]
-
-TAILWIND_APP_NAME = 'theme'
 
 INTERNAL_IPS = [
     "127.0.0.1",
